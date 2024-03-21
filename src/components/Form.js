122d1ef407925bad4a013f50cbd84b10d3dd3173
Anushka Verma <70652314+anushkaverma1340@users.noreply.github.com>
2024-03-21 07:05:21 +0530

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,18 +4,20 @@ const Form = ({ city, setCity, countryCode, setCountryCode, handleSubmit }) => {
   return (
     <form onSubmit={handleSubmit}>
       <input
+        style={{ margin: "10px" }}
         type="text"
         placeholder="Enter city"
         value={city}
         onChange={(e) => setCity(e.target.value)}
       />
       <input
+        style={{ margin: "10px" }}
         type="text"
         placeholder="Enter country code (ISO)"
         value={countryCode}
         onChange={(e) => setCountryCode(e.target.value)}
       />
-      <button type="submit">Get Forecast</button>
+      <button style={{ margin: "10px" }} type="submit">Get Forecast</button>
     </form>
   );
 };
